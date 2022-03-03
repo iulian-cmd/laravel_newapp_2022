9bb14bb0c4ccd215cc0573eb721732f08d55671f
hhendrik81 <hhouinou@gmail.com>
2022-03-03 13:47:06 +0100

    test model

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function showList()
     {
-        return view('product-list');
+        //requêtes sql brute -> retourne un tableau
+        // $products = DB::select('select * from product');
+        // dd($products);
+        //fluent query builder -> retourne un objet (collection)
+        // $products = DB::table('product')->get(); //retourne toute la table
+        // $products = DB::table('product')->first(); //retourne le premier produit
+        // dd($products);
+
+
+        //avec model
+        $products = Product::all();
+        dd($products);
+
     }
 
     public function showProduct($id)
