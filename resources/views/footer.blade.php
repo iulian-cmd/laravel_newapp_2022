e9b2baeedd41da2f831601c76a7bd8c5a293c37f
iulian-cmd <flysteelbird@gmail.com>
2022-03-03 13:43:33 +0100

    add footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,199 @@
+<!-- Footer -->
+<footer class="jumbotron page-footer font-small bg-light" style="width:100% !important;">
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+        <!-- Footer links -->
+        <div class="row text-center text-md-left pb-3">
+            <hr>
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold ">Boutique à VTT </h6>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus. Volutpat consequat mauris nunc
+                    congue. </p>
+            </div>
+            <!-- Grid column -->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold ">Nos produits</h6>
+                <p>
+                    <?php
+                      echo '<a href="index.php?page=catalogue.php"
+                        class="btn btn-outline-secondary btn-sm">VTTs</a>'
+                      ?>
+                </p>
+                <p>
+                    <a href="#" class="btn btn-outline-secondary btn-sm">Accessories</a> (à venir)
+                </p>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold ">Horaires</h6>
+
+                <table class="d-flex justify-content-center">
+                    <tbody>
+                        <tr>
+                            <th>Lundi</th>
+                            <td>Fermé</td>
+                        </tr>
+                        <tr>
+                            <th>Mardi</th>
+                            <td>10:00 - 19:00</td>
+                        </tr>
+                        <tr>
+                            <th>Mercredi&nbsp;</th>
+                            <td>10:00 - 19:00</td>
+                        </tr>
+                        <tr>
+                            <th>Jeudi&nbsp;</th>
+                            <td>10:00 - 19:00</td>
+                        </tr>
+                        <tr>
+                            <th>Vendredi&nbsp;</th>
+                            <td>10:00 - 19:00</td>
+                        </tr>
+                        <tr>
+                            <th>Samedi&nbsp;</th>
+                            <td>10:00 - 19:00</td>
+                        </tr>
+                        <tr>
+                            <th>Dimanche&nbsp;</th>
+                            <td>Fermé</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Grid column -->
+            <hr class="w-100 clearfix d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                <table class="d-flex justify-content-center">
+                    <tr class="d-flex justify-content-start">
+                        <th>
+                            <i class="bi bi-house-door"></i>
+                        </th>
+                        <td>
+                            <p>&nbsp;
+                                Chambery,
+                                73000, FR
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr class="d-flex justify-content-start">
+
+                        <th>
+                            <i class="bi bi-envelope"></i>
+                        </th>
+                        <td>
+
+
+                            <p>&nbsp;
+                                info@gmail.com
+                            </p>
+                    </tr>
+
+                    <tr class="d-flex justify-content-start">
+
+                        </td>
+                        <th>
+                            <i class="bi bi-telephone"></i>
+                        </th>
+                        <td>
+
+
+                            <p>&nbsp;
+                                + 33 01 234 567 88
+                            </p>
+                    </tr>
+                    <tr class="d-flex justify-content-start">
+
+                        </td>
+                        <th>
+                            <i class="bi bi-telephone-fill"></i>
+                        </th>
+                        <td>
+
+                            <p>&nbsp;
+                                + 33 01 234 567 89
+                            </p>
+                    </tr>
+                    </td>
+                </table>
+
+            </div>
+            <!-- Grid column -->
+            <hr class="w-100 clearfix d-md-none">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+
+            </div>
+            <!-- Grid column -->
+        </div>
+        <!-- Footer links -->
+
+        <hr>
+
+        <!-- Grid row -->
+        <div class="row d-flex align-items-center">
+            <p class="text-center text-md-center">© 2021 Copyright:
+                <strong> iulian </strong>
+            </p>
+            <!-- Grid column -->
+            <div class="col-md-7 col-lg-8">
+                <!--Copyright-->
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-5 col-lg-4 ml-lg-0">
+
+                <!-- Social buttons -->
+                <div class="text-center text-md-right">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                <i class="fab fa-google-plus-g"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -15,20 +15,23 @@
 
 @section('content')
 
+
     @foreach($products as $product)
     
-        <div class="card">
-        <h5 class="bg-light card-title">Name: {{$product->name}}</h5>
-        <img src="{{$product->image}}" class="card-img-top rounded m-3 p-2" alt="..." style="object-fit: contain;">
-        <div class="card-body">
-        <p class="bg-light card-text">Description: {{$product->description}}</p>
-        <p class="bg-light">Price: {{$product->price}}</p>
-        <p class="bg-light">Duration of the adventure: {{$product->duration}}</p>
-        <p class="bg-light">For clients aged over 14: {{$product->adult}}</p>
-        <p class="bg-light">Level of difficukty: {{$product->level}}</p>   
-        </div>
-      </div>
-    
-  @endforeach
+        <div class="card p-2 m-3 shadow p-3 mb-5 bg-body rounded">
+            <img src="{{$product->image}}" class="card-img-top shadow p-2 mb-3 bg-body rounded" alt="image_of_product" style="height: 15vw; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="bg-light card-title text-center mb-3"><span class="fw-bold text-secondary">Name of the product: </span><span class="text-uppercase fw-lighter text-center">{{$product->name}}</span></h5>
+                <p class="bg-light card-text"><span class="fw-bold text-center text-secondary">Description: </span><span class="fw-lighter">{{$product->description}}</span></p>
+                <p class="bg-light card-text"><span class="fw-bold text-center text-secondary">Price: EUR </span><span class="text-uppercase">{{$product->price}}</span></p>
+                <p class="bg-light card-text"><span class="fw-bold text-center text-secondary">Duration of the adventure: </span><span class="text-uppercase fw-lighter">{{$product->duration}}</span><span class="fw-bold text-center text-secondary"> h </span></p>
+                <p class="bg-light card-text"><span class="fw-bold text-center text-secondary">For clients aged over 14: </span><span class="text-uppercase fw-lighter">{{$product->adult}}</span></p>
+                <p class="bg-light card-text"><span class="fw-bold text-center text-secondary">Level of difficulty: </span><span class="fw-lighter">{{$product->level}}</span></p>   
+            </div>
+            </div>
+          
+        
+        @endforeach
+
 
 @endsection
