00174f6c6cc890a9729799f1ed8e6828597ef822
iulian-cmd <flysteelbird@gmail.com>
2022-03-10 14:11:31 +0100

    tailwind fixed

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -10,9 +10,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Asset&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
     <title>App Name - @yield('title')</title>
 </head>
 
