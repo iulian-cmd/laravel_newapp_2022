2a63db1f7b3bea3047509bd953b7fdfac2d7b4fd
iulian-cmd <flysteelbird@gmail.com>
2022-03-10 14:33:21 +0100

    tailwind fixed 2

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -10,11 +10,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Asset&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/css/custom.css') }}" rel="stylesheet">
     <title>App Name - @yield('title')</title>
 </head>
 
-<body id="body" class="antialiased text-dark bg-light">
+<body id="bod" class="antialiased text-dark bg-light">
