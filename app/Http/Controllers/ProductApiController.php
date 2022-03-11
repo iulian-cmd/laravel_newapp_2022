d33b39e7b4cf809a2c4ee95764c106a397c13ad7
hhendrik81 <hhouinou@gmail.com>
2022-03-11 15:58:27 +0100

    create api

--- app/Http/Controllers/ProductApiController.php
+++ app/Http/Controllers/ProductApiController.php
@@ -14,7 +14,7 @@ class ProductApiController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -25,40 +25,42 @@ class ProductApiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Product::create($request->all());
+        return response()->json('Product add: success');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\ProductApi  $productApi
+     * @param  \App\Models\Product $productApi
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $productApi)
+    public function show(int $id)
     {
-        //
+        return response()->json(Product::findOrFail($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\ProductApi  $productApi
+     * @param  \App\Models\Product $productApi
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Product $productApi)
     {
-        //
+        $productApi->update($request->all());
+        return response()->json('Product update: success');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\ProductApi  $productApi
+     * @param  \App\Models\Product $productApi
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $productApi)
     {
-        //
+        $productApi->delete();
     }
 }
